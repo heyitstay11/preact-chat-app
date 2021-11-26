1531fbcb84d2b73f8b30346c5e0421be4eab48d5
Tayyab <heyitstay011@gmail.com>
2021-11-26 16:56:48 +0530

    Fixed Loader

--- src/components/loading/index.jsx
+++ src/components/loading/index.jsx
@@ -1,8 +1,9 @@
 import './style.css'
+import loaderImg from '../../favicon.svg'
 
 const Loader = () => (
     <div class="loader flex flex-col items-center justify-center min-w-full">
-        <img class="loader-img" src="/src/favicon.svg" height="200" width="200" alt="" />
+        <img class="loader-img" src={loaderImg} height="200" width="200" alt="" />
     </div>
 )
 
