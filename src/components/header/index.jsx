aac11f3e9a42eebd2c57942b65fc5a9c689327cb
Tayyab <heyitstay011@gmail.com>
2022-02-07 19:07:51 +0530

    Home Page

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,14 +1,18 @@
 import { Link } from 'preact-router/match';
 import { useAuthContext } from '../../authContext';
+import logo from '../../favicon.svg';
 
 const Header = () => {
     const { user } = useAuthContext();
 
 return (
-    <header className="bg-purple-700 text-white flex flex-wrap items-center justify-between px-4 py-2">
-        <div class="brand text-2xl">Chat App</div>
+    <header className="bg-purple-700 text-white flex flex-wrap items-center justify-between px-4 py-3">
+        <div class="brand text-3xl flex px-2">
+            <img src={logo} alt="" height="30" width="30"  />    
+            <span className='ml-2'>Chat App</span>
+        </div>
         <nav>
-            <ul className="flex px-4 text-lg">
+            <ul className="flex px-4 text-xl">
                 <li ><Link className="text-white no-underline hover:opacity-90" href="/">Home</Link></li>
                 <li className="pl-6"><Link className="text-white no-underline hover:opacity-90" href="/join">Quick Room</Link></li>
                 {(user.email && user.token) ? null : (
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -68,41 +68,54 @@ const Home = () => {
   });
 
 return (
-  <div class="mx-auto max-w-2xl text-center px-2">
-    <h1 class="text-4xl font-400 my-4">Chat App</h1>
+  <div class="px-2" >
     {user.email ? (
-      <>
-      <div class="flex items-center justify-between my-5">
-        <p class="text-lg">You are logged in as {user.email}</p>
-        <button class="text-lg button" onClick={handleLogout}>Logout</button>
+      <div class={`lg:w-4/5 mx-auto text-center px-2`} >
+          <div class="flex items-center justify-between my-5">
+            <p class="text-lg">You are logged in as {user.email}</p>
+            <button class="text-lg py-2 px-6 font-500 button" onClick={handleLogout}>Logout</button>
+          </div>
+          <form onSubmit={handleSearch} class="flex items-center">
+            <label class="text-xl" htmlFor="search">Search friend</label>
+            <input class="ml-4" value={search} onInput={(e) => setSearch(e.target.value)} placeholder="john@doe.com" type="email" name="search" id="search" />
+          </form>
+          {(searchResult.email && searchResult.email !== user.email)? (
+            <div class="flex my-4 mx-4 items-center justify-around text-left">
+            <p class="mx-4 text-lg" >Username: {searchResult.username}</p>
+            <p class="font-bold">Email: {searchResult.email}</p>
+            <button class="button" onClick={handleAdd} >Add</button>
+          </div>
+          ) : null}
+          <div class="h-3px w-full bg-purple-700 mt-6 mb-2"></div>
+          <div class=" flex flex-col text-left my-2 ml-2">
+          {(channels && channels.length > 0 )&& (<h2 class="text-2xl font-500 mt-2 mb-1">Chats</h2>)}
+          {(channels && channels.length) > 0 &&
+              channels.map((channel) => {
+                return <a class="button mt-2 w-min text-lg px-6 py-2" href={`/channel/${channel._id}`}>
+                          {channel.title.replace(user.name, '')}
+                      </a>
+          })}
+          </div>
       </div>
-      <form onSubmit={handleSearch} class="flex items-center">
-        <label class="text-xl" htmlFor="search">Search friend</label>
-        <input class="ml-4" value={search} onInput={(e) => setSearch(e.target.value)} placeholder="john@doe.com" type="email" name="search" id="search" />
-      </form>
-      {(searchResult.email && searchResult.email !== user.email)? (
-        <div class="flex my-4 mx-4 items-center justify-around text-left">
-        <p class="mx-4 text-lg" >Username: {searchResult.username}</p>
-        <p class="font-bold">Email: {searchResult.email}</p>
-        <button class="button" onClick={handleAdd} >Add</button>
-      </div>
-      ) : null}
-      <div class="h-3px w-full bg-purple-700 mt-6 mb-2"></div>
-      <div class=" flex flex-col text-left my-2 ml-2">
-      {(channels && channels.length > 0 )&& (<h2 class="text-2xl font-500 ">Chats</h2>)}
-      {(channels && channels.length) > 0 &&
-          channels.map((channel) => {
-            return <a class="button mt-2 w-min" href={`/channel/${channel._id}`}>
-                      {channel.title.replace(user.name, '')}
-                   </a>
-      })}
-      </div>
-      </>
       ) :
-      <>
-        <p class="my-2 text-2xl max-w-40ch mx-auto" >Create a quick chat session using a username, room name</p>
-        <Link href="/join" class="inline-block no-underline text-lg my-2 text-white bg-purple-700 hover:bg-purple-900 px-4 py-2 rounded-sm hover:bg-purple-900 "  aria-label="Visit the Join/Chat page of site" >Create/Join</Link>
-      </>}
+      <section class="text-gray-600">
+        <div class="container mx-auto flex px-5 py-20 md:flex-row flex-col items-center">
+          <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 class="sm:text-6xl text-4xl mb-4 font-medium text-gray-900">Connect with your
+              <br class="hidden lg:inline-block" /> friends
+            </h1>
+            <p class="mb-8 leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sunt similique magnam quo quae nesciunt dolor, optio culpa dolore amet </p>
+            <div class="flex justify-center">
+              <a href='/signup' class="inline-flex text-white bg-purple-700 border-0 py-3 px-7 focus:outline-none hover:bg-purple-600 rounded text-lg button">Sign up</a>
+              <a href='/join' class="ml-4 inline-flex text-purple-700 bg-gray-100 py-3 px-7 focus:outline-none hover:bg-gray-200 rounded text-lg border-2 border-purple-700">Join</a>
+            </div>
+          </div>
+          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            <img class="object-cover object-center rounded" alt="hero" src="./public/phone.png" />
+          </div>
+        </div>
+      </section>
+  }
   </div>
 );
 }
